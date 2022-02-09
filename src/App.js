e9b5eb41331da9ab4de69ca4b7a006ff11e88e5b
ParkSangmin <tablemin_park@daum.net>
2022-02-10 00:46:13 +0900

    6.0 Introduction ~ 6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+
+import { useState, useEffect } from "react";
+
+// useEffect(effect: [한번만 실행하고싶은 코드], deps?: [한번만 실행하고 싶은 코드를 실행할 트리거 state])
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  // 실행시 한번만 실행된다.
+  useEffect(() => {
+    console.log("I run only once.")
+  }, []);     
+  
+  // counter state가 변화할때만 실행된다.
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");  
+  }, [counter]);
+
+  // keyword state가 변화할때만 실행된다.
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);    
+
+  // counter, keyword state가 변화할때만 실행된다.
+  useEffect(() => {
+    console.log("I run when 'counter' and 'keyword' changes.");
+  }, [counter, keyword]);    
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
-// js 파일마다 각각 css파일을 연결해주기 때문에 하나의 css파일로 광역적용을 시킬 필요가 없다.
-// 클래스이름이 랜덤하게 적용되기 때문에 다른 css파일끼리 클래스명이 중복되어도 상관이 없다.
-
-// 실행결과
-// <h1 class="App_title__+IZYT">Welcome back!!!</h1>
-// <button class="Button_btn__pba4O">Continue</button>
-
+// state이 변경될 때 컴포넌트의 모든 코드가 실행된다. (counter의 값이 바뀔 때마다 새로고침된다.)
 export default App;
