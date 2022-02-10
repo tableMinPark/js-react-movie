a7ebe8063dc585fd6eafc666860be5f20f6bbba1
ParkSangmin <tablemin_park@daum.net>
2022-02-11 01:37:33 +0900

    7.8 Conclusions ~ 7.9 Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,25 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 // Link를 사용하면 페이지 간 이동을 할 수있다 (새로고침없이)
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg}></img>
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>    
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img src={coverImg} className={styles.movie__img}></img>
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,6 @@
-import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; // URL 파라미터로 넘거준 data를 받을 수 있다.
+import styles from "./Detail.module.css";
 
 function Detail() {
   const { id } = useParams(); // URL 파라미터로 넘어온 data를 저장 : {id: '[id의 값]'}
@@ -18,29 +18,33 @@ function Detail() {
     getMovies();
   }, []);
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading movie detail....</h1>
+        <div className={styles.loader}>
+          <h1>Loading movie detail....</h1>
+        </div>
       ) : (
-        <div>
-          <img src={detail.large_cover_image}></img>
-          <hr/>
-
-          <h1>{detail.title_long}</h1>
-          <h5>Rating : {detail.rating} / Runtime : {detail.runtime}m</h5>
-          <ul>
-            {detail.genres.map((g) => (
-              <li key={g}><h5>{g}</h5></li>
-            ))}
-          </ul>
-          <hr/>
-          
-          <h3>Description</h3>
-          <p>{detail.description_full}</p>
+        <div className={styles.movie}>
+          <img src={detail.large_cover_image} className={styles.movie__img}></img>
+          <div>
+            <h1 className={styles.movie__title}>
+              {detail.title}
+            </h1>
+            <h3 className={styles.movie__year}>{detail.year}</h3>
+            <ul className={styles.movie__genres}>
+              {detail.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+            <h3 className={styles.movie__rating}>
+              ★ {detail.rating}
+            </h3>
+            <hr/>
+            <p>{detail.description_full}</p>
+          </div>
         </div>
       )}
     </div>
   );
 }
 export default Detail;
-
