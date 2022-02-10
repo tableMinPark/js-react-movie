80a8ac0a273d0c035216b9f1c9bf9190925de3ed
ParkSangmin <tablemin_park@daum.net>
2022-02-10 17:29:10 +0900

    7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";     // npm install react-router-dom@5.3.0
 
 function App() {
-  return null;
+  // 페이지간 이동 경로를 정의 "/" 이 가장 마지막에 있어야한다. : path의 앞부분 부터 일치하는 것이 있으면 이동해버리기 때문에 "/" 이 가장 위에있으면 "/movie" 로 넘어갈수없다. `Route exact path="/"` 와 같이 완전일치 판정으로 바꾸면 순서가 상관이 없다.
+  
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+// Link를 사용하면 페이지 간 이동을 할 수있다 (새로고침없이)
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg}></img>
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>    
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -16,10 +21,10 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
-  coverImg:  PropTypes.string.isRequired,
+  coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+};
 
 export default Movie;
